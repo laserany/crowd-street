55935a619835eba07cc1f7f82573aff9ebb39f6a
laserany <dotachessfan@outlook.com>
2020-11-22 17:22:34 -0600

    added jasypt for encrypting secrets

---
+++ src/test/java/com/crowdstreet/backend/configuration/BackendConfigurationTest.java
@@ -0,0 +1,29 @@
+package com.crowdstreet.backend.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BackendConfigurationTest {
+
+    @Autowired
+    ApplicationContext applicationContext;
+    @Autowired
+    Environment environment;
+
+    @Test
+    public void assertThatThirdServiceBeanExists() {
+        assertNotNull(applicationContext.getBean(ThirdService.class));
+    }
+
+    @Test
+    public void assertThatSecretValuesAreDecryptedProperly() {
+        assertEquals("test-access-key", environment.getProperty("aws.access.key.id"));
+        assertEquals("test-secret-key", environment.getProperty("aws.secret.access.key"));
+    }
+}
